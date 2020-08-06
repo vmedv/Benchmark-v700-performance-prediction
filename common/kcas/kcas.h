a88af6e4385f2c46b2fd7499116eab0bf307c114
William Sigouin <wpsigoui@jax.multicore.cs.uwaterloo.ca>
2020-08-06 09:54:59 -0400

    added new validation KCAS interfaces to setbench

--- common/kcas/kcas.h
+++ common/kcas/kcas.h
@@ -3,10 +3,10 @@
 #define CASWORD_BITS_TYPE casword_t
 
 #ifndef likely
-    #define likely(x)       __builtin_expect((x),1)
+#define likely(x)       __builtin_expect((x),1)
 #endif
 #ifndef unlikely
-    #define unlikely(x)     __builtin_expect((x),0)
+#define unlikely(x)     __builtin_expect((x),0)
 #endif
 
 template <typename T>
@@ -24,27 +24,37 @@ public:
 
     T getValue();
 
+    casword_t getValueUnsafe(bool &isPtr);
+
     void addToDescriptor(T oldVal, T newVal);
 };
 
 #if defined KCAS_LOCKFREE
-    #include "kcas_reuse_impl.h"
+#include "kcas_reuse_impl.h"
 #elif defined KCAS_HTM
-    #include "kcas_reuse_htm_impl.h"
+#include "kcas_reuse_htm_impl.h"
 #elif defined KCAS_HTM_FULL
-    #include "kcas_reuse_htm_full_impl.h"
+#include "kcas_reuse_htm_full_impl.h"
+#elif defined KCAS_VALIDATE
+#include "kcas_validate.h"
+#elif defined KCAS_VALIDATE_HTM
+#include "kcas_validate_htm.h"
 #else
-    #error must define one of KCAS_LOCKFREE KCAS_HTM KCAS_HTM_FULL
+#error must define one of KCAS_LOCKFREE KCAS_HTM KCAS_HTM_FULL
 #endif
 
 namespace kcas {
-#if defined KCAS_LOCKFREE
+    #if defined KCAS_LOCKFREE
     KCASLockFree<MAX_KCAS> instance;
-#elif defined KCAS_HTM
+    #elif defined KCAS_HTM
     KCASHTM<MAX_KCAS> instance;
-#elif defined KCAS_HTM_FULL
+    #elif defined KCAS_HTM_FULL
     KCASHTM_FULL<MAX_KCAS> instance;
-#endif
+    #elif defined KCAS_VALIDATE
+    KCASValidate<MAX_KCAS> instance;
+    #elif defined KCAS_VALIDATE_HTM
+    KCASValidateHTM<MAX_KCAS> instance;
+    #endif
 
     void writeInitPtr(casword_t volatile * addr, casword_t const newval) {
         return instance.writeInitPtr(addr, newval);
@@ -66,12 +76,13 @@ namespace kcas {
         return instance.execute();
     }
 
-    kcasptr_t getDescriptor() {
+    inline kcasptr_t getDescriptor() {
         return instance.getDescriptor();
     }
 
     bool start() {
-        return instance.start();
+        instance.start();
+        return true;
     }
 
     template<typename T>
@@ -83,6 +94,28 @@ namespace kcas {
     void add(casword<T> * caswordptr, T oldVal, T newVal, Args... args) {
         instance.add(caswordptr, oldVal, newVal, args...);
     }
+
+    #if defined KCAS_VALIDATE || defined KCAS_VALIDATE_HTM
+
+    inline bool validate()
+    {
+        return instance.validate();
+    }
+
+
+    inline bool validateAndExecute()
+    {
+        return instance.validateAndExecute();
+    }
+
+    template <typename NodePtrType>
+    inline casword_t visit(NodePtrType node)
+    {
+        assert(node != NULL);
+        return instance.visit(node);
+    }
+    #endif
+
 
 };
 
