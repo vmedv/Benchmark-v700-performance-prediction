ca7cf9855b1b2c098cb8a1663ada1afe48ba1221
Aleksandr Slastin <a.slastin@bk.ru>
2023-08-04 23:54:44 +0300

    exp table

--- plotting/experiments_table_builder.py
+++ plotting/experiments_table_builder.py
@@ -1,13 +1,51 @@
 import re 
 from pathlib import Path
 import argparse
+from statistics import variance 
 
 DEFAULT_OUTPUT_FILE_NAME = "experiments-table"
 
-PATTERN = r"avg_val=(\S*)"
+PATTERN_AVG_VAL = r"avg_val=(\S*)"
+PATTERN_VALUES = r"values=\[(.*?)\]"
 COLUMN_SEP = " & "
 ROUND = 2
 ROW_SEP = "\n\\hline\n"
+PERCENT = 0.95
+COLOR = "blue!20"
+
+INDEX = 2
+
+BASE_DS = {"btree", "ist"}
+
+DS_NAME_MAP = {
+    "splay_tree": "Splay Tree",
+    "sast": "SA2T",
+    "btree": "B-Tree",
+    "sabt": "SABT",
+    "ist": "IST",
+    "sait": "SAIT",
+    "salt": "SALT"
+}
+
+WORKLOAD_NAME_MAP = {
+    "70-30": "70/30",
+    "80-20": "80/20",
+    "90-10": "90/10",
+    "95-05": "95/05",
+    "99-01": "99/01"
+}
+
+def get_workload_name(workload):
+    return WORKLOAD_NAME_MAP[workload] if workload in WORKLOAD_NAME_MAP else workload
+
+
+def format_main_ops(ops):
+    sops = '%.2g' % ops
+    pos = sops.find("e+0")
+    if pos == -1:
+        raise RuntimeError("can not find e+0 in " + sops)
+    return sops[:pos] + " \cdot {10}^{" + sops[pos+3:pos+4] + "}"
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -26,6 +64,7 @@ if __name__ == "__main__":
             workload = workload_dir.name
             if workload not in args.workloads:
                 continue
+            # print("workload=", workload)
             for query_dir in workload_dir.iterdir():
                 if query_dir.name == args.query:
                     for ds_dir in query_dir.iterdir():
@@ -35,24 +74,48 @@ if __name__ == "__main__":
                         for stat_file in ds_dir.iterdir():
                             if args.stat in stat_file.name:
                                 content = stat_file.read_text()
-                                all = re.findall(PATTERN, content)
-                                workloads_by_ds[ds][workload] = float(all[2])
+                                all = re.findall(PATTERN_AVG_VAL, content)
+                                # values = list(map(float, all[INDEX].split(", ")))
+                                # print("ds=", ds, "\tvar=", variance(values))
+                                workloads_by_ds[ds][workload] = float(all[INDEX])
 
     # print(workloads_by_ds)
 
-    table = []
-    table.append("\\begin{tabular}{|" + " | ".join(["c" for _ in range(len(args.workloads) + 1)]) + "|}")
-    table.append(COLUMN_SEP + COLUMN_SEP.join(args.workloads) + "\\\\")
-
-    pivot_dict = workloads_by_ds[args.ds[0]]
+    values_by_ds = {}
+    base_best_dict = {}
 
     for i, ds in enumerate(args.ds):
         ds_dict = workloads_by_ds[ds]
+        values = []
+        for workload in args.workloads:
+            value = ds_dict[workload]
+            if ds in BASE_DS:
+                if workload in base_best_dict:
+                    base_best_dict[workload] = max(base_best_dict[workload], value)
+                else:
+                    base_best_dict[workload] = value
+            values.append(value)
+        values_by_ds[ds] = values
+
+    table = []
+    table.append("\\begin{tabular}{|" + " | ".join(["c" for _ in range(len(args.workloads) + 1)]) + "|}")
+    table.append(COLUMN_SEP + COLUMN_SEP.join(map(get_workload_name, args.workloads)) + "\\\\")
+
+    for i, ds in enumerate(args.ds):
+        ds_dict = workloads_by_ds[ds]
+        values = values_by_ds[ds]
         if i == 0:
-            values = [f"${ds_dict[workload]}$" for workload in args.workloads]
+            pivot_dict = ds_dict
+            row = [f"${format_main_ops(ds_dict[workload])}$" for workload in args.workloads]
         else:
-            values = [f"x${round(ds_dict[workload] / pivot_dict[workload], ROUND)}$" for workload in args.workloads]
-        table.append(ds + COLUMN_SEP + COLUMN_SEP.join(values) + " \\\\")
+            row = []
+            for workload, value in zip(args.workloads, values):
+                percent = round(ds_dict[workload] / pivot_dict[workload], ROUND)
+                cell = f"x${percent}$"
+                if ds not in BASE_DS and value >= PERCENT * base_best_dict[workload]:
+                    cell = "\\cellcolor{" + COLOR + "}" + cell
+                row.append(cell)
+        table.append(DS_NAME_MAP[ds] + COLUMN_SEP + COLUMN_SEP.join(row) + " \\\\")
     
     table.append("\\end{tabular}")
     
