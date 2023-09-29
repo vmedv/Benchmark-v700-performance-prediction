e653df8caf310f3fbf6a423fd38b98717c0b6b8b
Aleksandr Slastin <a.slastin@bk.ru>
2023-09-29 17:40:21 +0000

    remove gsat trees

--- gsat/cmake/TestSolution.cmake
+++
@@ -1,4 +0,0 @@
-function(add_catch TARGET)
-    add_executable(${TARGET} ${ARGN})
-    target_link_libraries(${TARGET} contrib_catch_main)
-endfunction()
--- gsat/test/util/operation.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-constexpr int kCntOperation = 4;
-
-enum class Operation { kFind, kContains, kInsert, kDelete };
